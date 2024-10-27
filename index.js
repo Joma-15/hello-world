826f76be52f8ea1125b22373372f09c56e321550
Jhon Marcel Adelantar <adelzarajhonmarcel@gmail.com>
2024-10-27 18:25:17 +0800

    using the call method

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-let value = this; 
-console.log(value)
+const user1 = {
+    userdata: function(){
+        //the this will be used as reference to the object 
+        return `your username is ${this.username} and your password is ${this.password}`; 
+    }
+}
+//creating another object 
+const user2 = {
+    //adding two key value pairs like the first function 
+    username: "jhon marcel adelantar",
+    password: "uzumaki@15"
+}
+console.log(user1.userdata.call(user2));
