94ccc0d7e820662b4080cc9fd699151ee08504d1
Jhon Marcel Adelantar <adelzarajhonmarcel@gmail.com>
2024-10-27 18:17:19 +0800

    using the this keyword

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-x = 5; 
-console.log(x); //this will print out the value
-//the variable in js can be used before it has been declared
-var x; //declaring the variable
+let value = this; 
+console.log(value)
