e030aa9f23da5fbc28af52cd8f1f2699938e78c1
Jhon Marcel Adelantar <adelzarajhonmarcel@gmail.com>
2024-10-27 18:00:49 +0800

    learning about hoisting

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-function myfunction(){
-    let carName = "Mitsubishi"; 
-}
+x = 5; 
+console.log(x); //this will print out the value
+//the variable in js can be used before it has been declared
+var x; //declaring the variable
