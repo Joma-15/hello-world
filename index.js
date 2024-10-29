12e7af2f1aa0ddf224eef4304b2108debed8b4b7
Jhon Marcel Adelantar <adelzarajhonmarcel@gmail.com>
2024-10-29 19:03:20 +0800

    learned about inheritance and learned about getters and setters

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-//in javascript we can nest a fuckin function 
-const sayHello =(function () {
-    let text = ""; 
-    return function () {
-        text += "hello world"; 
-        return text;
-    }
-})();
+// //concept of inheritance in javascriipt
+// class Parent{
+//     constructor(){
+//         //it cannot be accessed because it is defined as a local variable
+//         //it should be define as a this to treat it as a property
+//         const name = "hello world";
+//         this.usename = "hi world";
+//     }
+// }
+// class Child extends Parent{
+//     constructor(){
+//         super();
+//     }
+// }
+// const anak = new Child();
+// console.log(anak.name);
+// console.log(anak.usename)
 
-console.log(sayHello());
+//getters and setters in javascript 
+class UserData{
+    constructor(username,password){
+        this.username = username;
+        this.password = password;
+    }
+    //getters 
+    get usename(){
+        return this.username;
+    }
+    //setters 
+    set usename(x){
+        this.username = x;
+    }
+}
+//crfeating an instance of the object 
+const user = new UserData("Marcel","uzumaki@15");
+user.usename = "hello world"; 
+console.log(user.usename);
