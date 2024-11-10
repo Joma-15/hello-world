0d30051329f4ee8633aec12567dd1983974d912a
Jhon Marcel Adelantar <adelzarajhonmarcel@gmail.com>
2024-11-10 20:40:03 +0800

    hello world

--- index.js
+++
@@ -1,7 +0,0 @@
-let obj = {username: "jhon marcel Adelantar", password: "uzumaki@15"}; 
-
-
-//loop through key value pairs 
-for(let [key, value] of Object.entries(obj)){
-    console.log(`Hello my ${key} is ${value}`)
-}
