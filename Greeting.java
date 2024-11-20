bd6de2da89b6b4458b6ec5bb40aff7adf2d27a02
Jhon Marcel Adelantar <adelzarajhonmarcel@gmail.com>
2024-11-20 19:50:34 +0800

    using the heap and the collections package in java

--- Greeting.java
+++ Greeting.java
@@ -1,30 +1,16 @@
-import java.util.Scanner;
-import java.util.PriorityQueue;;
+import java.util.ArrayList;//heap can be represented using arraylist in java
+import java.util.Collections;
 
 public class Greeting {
-    static Scanner scanner = new Scanner(System.in); 
-    static PriorityQueue<String> nicknames = new PriorityQueue<>();
 
     public static void main(String[] args) {
-
-        System.out.println("Enter the nicknames of 4 of your classmates: ");
-        for (int i = 0; i < 4; i++) {
-            nicknames.add(scanner.nextLine()); //will add the name of the classmate
+        ArrayList<Integer> heap = new ArrayList<>(); 
+        
+        //adding some element in the heap 
+        Collections.addAll(heap, 1,2,3,4,5); 
+        //determinign where is the root node 
+        for (Integer e : heap) {
+            System.out.println(e);
         }
-        System.out.println("\nPress H to say Hi to each of them H : ");
-        sayHi();
-
-    }
-    static void sayHi(){
-        while (!nicknames.isEmpty()) {
-            boolean isValidRes = scanner.nextLine().equalsIgnoreCase("H");
-
-            if (isValidRes) {
-                System.out.println("Hi " + nicknames.poll());
-            }else{
-                System.out.println("invalid input try again!");
-            }
-        }
-        System.out.println("Done saying Hi");
     }
 }
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,13 @@
-import PromptSync from "prompt-sync";
+function getData(){
+    document.getElementById("demo").innerHTML = "hello world"; 
+    async function storedData(){
+        const response = await fetch("hello world.txt"); 
 
-//instantiating the object 
-const prompt = PromptSync(); 
-prompt("hello what is your name : ")
+        //check if the response is ok or not 
+        if(!response.ok()){
+            throw new console.error("cannot fetch data in the server");
+        }
+        const data = await response.json()//converting the data into a json object 
+        console.log(data);
+    }
+}
